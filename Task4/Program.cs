12d4215bcf8dafd6eb198d0d3804601fcca803e7
Asad-yokubov <for.home.work.2024@gmail.com>
2024-09-02 14:08:30 +0500

    add lesson1:Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 9;
+int b = 7;
+int c = 4;
+int d = 10;
+int e = 6;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
